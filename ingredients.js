7769f3690f010c64a3c8af30cafb0ff8389a259f
paigelindahl <paigemarlindahl@gmail.com>
2020-08-23 17:22:40 +0000

    practicing arrays and loops

---
+++ ingredients.js
@@ -0,0 +1,4 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for ( let i = 7; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
