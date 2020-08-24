10a5689d50b5e8e94130ac63d9552463a1ca4c8c
paigelindahl <paigemarlindahl@gmail.com>
2020-08-24 15:10:20 +0000

    objects practice

---
+++ smartgarbage.js
@@ -0,0 +1,16 @@
+bins = {
+  waste: 0,
+  recycling: 0,
+  compost: 0
+}
+function smartGarbage (trash, bins) {
+  if (trash === 'waste') {
+    bins.waste += 1;
+  } else if (trash === 'recycling') {
+    bins.recycling += 1;
+  } else if (trash === 'compost') {
+    bins.compost +=1;
+  }
+  return bins;
+}
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
